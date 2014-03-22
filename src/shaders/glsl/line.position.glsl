1e57afc8c181bb84996826fc0a0f0029f5866bf1
Steven Wittens <steven@acko.net>
2014-03-22 16:43:37 -0700

    optimize line shader

--- src/shaders/glsl/line.position.glsl
+++ src/shaders/glsl/line.position.glsl
@@ -16,46 +16,50 @@ vec3 getLineJoin(float edge, vec3 left, vec3 center, vec3 right) {
   vec2 join = vec2(1.0, 0.0);
 
   if (center.z < 0.0) {
-    if (edge > 0.5) {
-      vec4 a = vec4(left.xy, center.xy);
-      vec4 b = a / vec4(left.zz, center.zz);
-      vec2 nl = normalize(b.xy - b.zw);
-      vec2 tl = vec2(nl.y, -nl.x);
+    vec4 a = vec4(left.xy, right.xy);
+    vec4 b = a / vec4(left.zz, right.zz);
 
-      join = tl;
-    }
-    else if (edge < -0.5) {
-      vec4 a = vec4(center.xy, right.xy);
-      vec4 b = a / vec4(center.zz, right.zz);
-      vec2 nr = normalize(b.xy - b.zw);
-      vec2 tr = vec2(nr.y, -nr.x);
+    vec2 l = b.xy;
+    vec2 r = b.zw;
+    vec2 c = center.xy / center.z;
 
-      join = tr;
+    vec4 d = vec4(c, l) - vec4(r, c);
+    float l1 = dot(d.xy, d.xy);
+    float l2 = dot(d.zw, d.zw);
+
+    if (l1 + l2 > 0.0) {
+      
+      if (edge > 0.5 || l1 == 0.0) {
+        vec2 nl = normalize(l - c);
+        vec2 tl = vec2(nl.y, -nl.x);
+
+        join = tl;
+      }
+      else if (edge < -0.5 || l2 == 0.0) {
+        vec2 nr = normalize(c - r);
+        vec2 tr = vec2(nr.y, -nr.x);
+
+        join = tr;
+      }
+      else {
+        vec2 nl = normalize(d.xy);
+        vec2 nr = normalize(d.zw);
+
+        vec2 tl = vec2(nl.y, -nl.x);
+        vec2 tr = vec2(nr.y, -nr.x);
+
+        vec2 tc = normalize(tl + tr);
+      
+        float cosA = dot(nl, tc);
+        float sinA = max(0.1, abs(dot(tl, tc)));
+        float factor = cosA / sinA;
+        float scale = sqrt(1.0 + factor * factor);
+
+        join = tc * scale;
+      }
     }
     else {
-      vec4 a = vec4(left.xy, right.xy);
-      vec4 b = a / vec4(left.zz, right.zz);
-
-      vec2 l = b.xy;
-      vec2 r = b.zw;
-      vec2 c = center.xy / center.z;
-
-      vec4 d = vec4(c, l) - vec4(r, c);
-
-      vec2 nl = normalize(d.xy);
-      vec2 nr = normalize(d.zw);
-
-      vec2 tl = vec2(nl.y, -nl.x);
-      vec2 tr = vec2(nr.y, -nr.x);
-
-      vec2 tc = normalize(tl + tr);
-      
-      float cosA = dot(nl, tc);
-      float sinA = max(0.1, abs(dot(tl, tc)));
-      float factor = cosA / sinA;
-      float scale = sqrt(1.0 + factor * factor);
-
-      join = tc * scale;
+      return vec3(0.0);
     }
   }
     
