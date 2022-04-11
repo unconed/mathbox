0598b582d216507911964f044dc1b41f5d812824
Christopher Chudzicki <christopher.chudzicki@gmail.com>
2022-04-10 22:52:03 -0400

    do not use gl_FrontFacing
    gl_FrontFacing seems to be causing an issue with shaded surfaces. Some data:
      - using `side = gl_FrontFacing ? -1.0 : 1.0` worked in mathbox-0.0.5 (Also on znah's fork using r84)
      - using `side = glFrontFacting ? -1.0 : 1.0` appears to be rendering both sides of a DoubleSided surface as "dark" (the back side) on current mathbox.
      - switching to `side = 1.0` renders both sides as light; `side = -1.0` renders both sides as dark.
    
    The workaround in this commit requires a WebGL standard_derivatives extension. This workaround was suggested in https://www.reddit.com/r/glsl/comments/3q8kn2/gl_frontfacing_on_a_mac/
    
    I do not understand why gl_FrontFacing worked on mathbox 0.0.5 but not hte current version. Something to do with threejs upgrades? Unsure.

--- src/shaders/glsl/mesh.fragment.shaded.js
+++ src/shaders/glsl/mesh.fragment.shaded.js
@@ -1,4 +1,6 @@
-export default /* glsl */ `varying vec3 vNormal;
+export default /* glsl */ `
+#extension GL_OES_standard_derivatives : enable
+varying vec3 vNormal;
 varying vec3 vLight;
 varying vec3 vPosition;
 
@@ -16,7 +18,12 @@ vec4 getShadedColor(vec4 rgba) {
   vec3 light = normalize(vLight);
   vec3 position = normalize(vPosition);
   
-  float side    = gl_FrontFacing ? -1.0 : 1.0;
+  // Workaround to avoid gl_FrontFacing. See https://github.com/unconed/mathbox/pull/26
+  vec3 pdx = dFdx(vPosition);
+  vec3 pdy = dFdy(vPosition);
+  bool frontFacting = dot(vNormal, cross(pdx, pdy)) > 0.0;
+
+  float side = frontFacting ? 1.0 : -1.0;
   float cosine  = side * dot(normal, light);
   float diffuse = mix(max(0.0, cosine), .5 + .5 * cosine, .1);
   
