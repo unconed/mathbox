69e4a8ed1687bf14ceb411e7add64ef4b25718da
Christopher Chudzicki <christopher.chudzicki@gmail.com>
2022-04-17 08:36:37 -0400

    remove gulp-eslint

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,4 @@
 const gulp = require("gulp");
-const eslint = require("gulp-eslint");
 const concat = require("gulp-concat");
 
 const builds = {
@@ -10,15 +9,4 @@ const css = ["node_modules/shadergraph/build/*.css", "src/**/*.css"];
 
 gulp.task("css", function () {
   return gulp.src(css).pipe(concat(builds.css)).pipe(gulp.dest("./"));
-});
-
-gulp.task("lint", function () {
-  return (
-    gulp
-      // Define the source files
-      .src("src/**/*.js")
-      .pipe(eslint({}))
-      // Output the results in the console
-      .pipe(eslint.format())
-  );
 });
